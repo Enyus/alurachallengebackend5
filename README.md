560c2d04ce8065751ed879f321405ad274d2bace
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2022-10-25 12:15:19 -0300

    adicionando seeder

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# alurachallengebackend5
-Respositório para guardar os desafios do Alura Challenge BackEnd.
+# Alura Challenge Back-end 5 - Alura Flix (WIP)
+
+Este repositório guarda a minha versão do back-end do site Alura Flix, desenvolvido no Alura Challenge Back-End 5, entre 24/10 e XX/11/2022.
+
+| :placard: Vitrine.Dev |     |
+| -------------  | --- |
+| :sparkles: Nome        | **Alura Challenge Back-end 5 - Alura Flix**
+| :label: Tecnologias | NodeJs, Express, Prisma, SQLite, Hopscotch
+| :rocket: URL         | 
+
+<!-- Inserir imagem com a #vitrinedev ao final do link -->
+![](https://i.imgur.com/4BTHvAB.png#vitrinedev)
+
+## Detalhes do Projeto
+O projeto tem o objetivo de construir um back-end para o front do site Aluraflix. Resolvi utilizar as tecnologias NodeJs, Express, Prisma, SQLite e o Hopscotch como ambiente de testes.
+
+- Semana 1: Iniciar o banco de dados e criar requisições CRUD básicas.
+- Semana 2:
+- Semanas 3 e 4:
+
+## Para rodar o projeto
+- Faça o clone do repositório com `git clone https://github.com/Enyus/alurachallengefrontend5.git`
+- Faça a migration do prisma com `npx prisma migrate dev --name videos_table`
+- Faça o seed dos primeiros vídeos do banco de dados com `npx prisma db seed` (opcional)
+- Inicie o servidor com `npm run dev`
---
+++ prisma/seed.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const video1 = await prisma.video.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      titulo: "Ecossistema React JS com Charles Assunção",
+      descricao: "Fique por dentro das novas bibliotecas de React JS, o uso de componentes, as conhecidas Redux e GraphQL, além da discussão sobre o ciclo de vida no Front-End.",
+      url: "https://www.youtube.com/watch?v=F5As-8hWNIQ",
+      createdAt: new Date("2022-10-25T11:09:00"),
+      updatedAt: null,
+      deletedAt: null,
+    },
+  });
+
+  const video2 = await prisma.video.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      titulo: "Cultura do aprendizado com Sérgio Lopes",
+      descricao: "Saiba como a cultura do aprendizado pode melhorar em muitos aspectos o ambiente de trabalho da sua empresa, desde ser uma solução para a escassez de profissionais com experiência até o aprendizado mútuo entre as pessoas.",
+      url: "https://www.youtube.com/watch?v=j5Hwydgcnsk",
+      createdAt: new Date("2022-10-25T11:10:00"),
+      updatedAt: null,
+      deletedAt: null,
+    },
+  });
+
+  const video3 = await prisma.video.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      id: 3,
+      titulo: "Os MELHORES livros de tecnologia para ler em Programação com Roberta Arcoverde",
+      descricao: "Conheça os melhores livros de tecnologia para se aprender computação ou programação, sejam iniciantes ou avançados, para base acadêmica, prática de programar e/ou de carreira no mundo do desenvolvimento.",
+      url: "https://www.youtube.com/watch?v=RvWQQRjz1Pw",
+      createdAt: new Date("2022-10-25T11:11:00"),
+      updatedAt: null,
+      deletedAt: null,
+    },
+  });
+
+  console.log({ video1, video2, video3 });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
