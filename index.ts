cc7b1fc2ae2d2bf5cd0588489556668e13f47693
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2022-10-27 16:09:04 -0300

    adicionando cors

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import express  from 'express';
+import cors from 'cors';
 import videoRouter from './routes/videoRouter';
 
 const app = express();
 app.use(express.json());
+app.use(cors(
+    /* {
+        origin: "http://www.rocketseat.com.br"
+        Só aceita requisições deste front-end
+    }
+    */
+))
 
 app.use('/videos', videoRouter);
 
